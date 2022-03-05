a6b60d3f0cfc1a0732ab285f0f07695af498f31f
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-04 18:04:36 -0800

    commit tree fea8fd4bc04bb074265ebbd0fe6aefffb2d2e554

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
@@ -13,13 +18,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('racecar'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('robot'));
 }
 
 module.exports = isPalindrome;
